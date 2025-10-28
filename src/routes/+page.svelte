f2ca02a8022c5fd37edccebb5a1ed525770dc1a1
Lukas Wicke <wicke.lukas@gmail.com>
2025-10-28 16:01:33 +0100

    added dimensions to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,42 +11,58 @@
 			href: 'https://www.instagram.com/lukasdabbelju/',
 			alt: 'Photographer in Reflection',
 			id: '9f15afec-920f-4385-e8e2-088144713e00',
-			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/9f15afec-920f-4385-e8e2-088144713e00/public'
+			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/9f15afec-920f-4385-e8e2-088144713e00/public',
+			width: 1366,
+			height: 910
 		},
 		{
 			alt: 'Bridge under dark clouds',
 			id: '8fc8c582-f9bc-4b78-c3ef-6a5512c73400',
-			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/8fc8c582-f9bc-4b78-c3ef-6a5512c73400/public'
+			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/8fc8c582-f9bc-4b78-c3ef-6a5512c73400/public',
+			width: 1366,
+			height: 910
 		},
 		{
 			alt: 'candy',
 			id: '5e3020f1-8a3a-4fc9-fb42-78b4de8c8901',
-			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/5e3020f1-8a3a-4fc9-fb42-78b4de8c8901/public'
+			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/5e3020f1-8a3a-4fc9-fb42-78b4de8c8901/public',
+			width: 1366,
+			height: 764
 		},
 		{
 			alt: 'bronze woman stature',
 			id: 'df0aae74-56c4-4c03-fac7-e6a2f678ef00',
-			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/df0aae74-56c4-4c03-fac7-e6a2f678ef00/public'
+			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/df0aae74-56c4-4c03-fac7-e6a2f678ef00/public',
+			width: 910,
+			height: 1366
 		},
 		{
 			alt: 'little girl in red dress sticker',
 			id: '64d6da33-438d-4127-1dbd-7155fbd75f00',
-			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/64d6da33-438d-4127-1dbd-7155fbd75f00/public'
+			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/64d6da33-438d-4127-1dbd-7155fbd75f00/public',
+			width: 910,
+			height: 1366
 		},
 		{
 			alt: 'Colorful Rocks',
 			id: '59ffa716-d204-46c7-4901-5a4e077ea000',
-			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/59ffa716-d204-46c7-4901-5a4e077ea000/public'
+			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/59ffa716-d204-46c7-4901-5a4e077ea000/public',
+			width: 1366,
+			height: 910
 		},
 		{
 			alt: 'red ropes',
 			id: 'b436f869-88d7-422d-d450-e7246ff41d00',
-			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/b436f869-88d7-422d-d450-e7246ff41d00/public'
+			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/b436f869-88d7-422d-d450-e7246ff41d00/public',
+			width: 1366,
+			height: 910
 		},
 		{
 			alt: 'colorful skulls in a row',
 			id: '4c87dc53-9c79-495c-938d-ade3d5361c01',
-			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/4c87dc53-9c79-495c-938d-ade3d5361c01/public'
+			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/4c87dc53-9c79-495c-938d-ade3d5361c01/public',
+			width: 1366,
+			height: 764
 		}
 	];
 
@@ -56,7 +72,9 @@
 			href: '/admin',
 			alt: 'Bridge under dark clouds',
 			id: '8fc8c582-f9bc-4b78-c3ef-6a5512c73400',
-			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/8fc8c582-f9bc-4b78-c3ef-6a5512c73400/public'
+			src: 'https://imagedelivery.net/k1HQsavJfzZQlI3O3ZBS7Q/8fc8c582-f9bc-4b78-c3ef-6a5512c73400/public',
+			width: 1366,
+			height: 910
 		});
 	}
 </script>
